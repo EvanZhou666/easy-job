8cb48e65abbfdccf1b60b99a5f97a82291f83bb2
EvanZhou666 <34089714+EvanZhou666@users.noreply.github.com>
2025-06-21 20:57:37 +0800

    auto inject Quartz Properties

--- easy-job-standalone/src/main/java/com/qianlima/easyjob/autoconfigure/EasyJobAutoConfiguration.java
+++ easy-job-standalone/src/main/java/com/qianlima/easyjob/autoconfigure/EasyJobAutoConfiguration.java
@@ -25,20 +25,48 @@
 package com.qianlima.easyjob.autoconfigure;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.quartz.JobStoreType;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@AutoConfiguration
+import java.time.Duration;
+import java.util.Map;
+
+@AutoConfiguration(before = QuartzAutoConfiguration.class)
 @ComponentScan(basePackages = "com.qianlima.easyjob")
 @EnableJpaRepositories("com.qianlima.easyjob.auth.repository") // 明确指定包路径
 @EntityScan(value = {"com.qianlima.easyjob.auth.entity", "com.qianlima.easyjob.entity"}) // 确保实体类被扫描
 public class EasyJobAutoConfiguration {
 
-//    @Bean
-//    @ConditionalOnMissingBean
-//    public JobService jobService() {
-//        return new JobServiceImpl();
-//    }
+    private static final String SCHEDULER_NAME = "easy-job-scheduler";
+
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean
+    public QuartzProperties quartzProperties() {
+        QuartzProperties properties = new QuartzProperties();
+        properties.setJobStoreType(JobStoreType.MEMORY);
+        properties.setSchedulerName(SCHEDULER_NAME);
+        properties.setAutoStartup(true);
+        properties.setStartupDelay(Duration.ofSeconds(3));
+        properties.setWaitForJobsToCompleteOnShutdown(true);
+        properties.setOverwriteExistingJobs(true);
+        Map<String, String> map = properties.getProperties();
+        map.put("org.quartz.scheduler.instanceName",  SCHEDULER_NAME);
+        map.put("org.quartz.scheduler.instanceId", "AUTO");
+        map.put("org.quartz.threadPool.class", "org.quartz.simpl.SimpleThreadPool");
+        map.put("org.quartz.threadPool.threadCount", "64");
+        map.put("org.quartz.scheduler.idleWaitTime", "3000");
+        map.put("org.quartz.scheduler.batchTriggerAcquisitionFireAheadTimeWindow", "0");
+        map.put("org.quartz.jobStore.misfireThreshold", "60000");
+        map.put("org.quartz.scheduler.batchTriggerAcquisitionMaxCount", "16");
+        return properties;
+    }
 
 }
