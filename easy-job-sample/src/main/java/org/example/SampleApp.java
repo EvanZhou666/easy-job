49dff903942442ca90af4344477f7cea694aca69
EvanZhou666 <34089714+EvanZhou666@users.noreply.github.com>
2025-09-15 23:36:31 +0800

    v1.0.1 released

--- easy-job-sample/src/main/java/org/example/SampleApp.java
+++ easy-job-sample/src/main/java/org/example/SampleApp.java
@@ -26,8 +26,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SampleApp {
     public static void main(String[] args) {
         SpringApplication.run(SampleApp.class, args);
